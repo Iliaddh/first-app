b72b80a8166502cafbc99ee18735f8a2f455a777
Iliad Hemmatjoo <ilyadhemmatjoo2003@gmail.com>
2023-12-25 22:21:57 -0500

    responsive- styled components

--- src/Components/Card.js
+++ src/Components/Card.js
@@ -1,6 +1,47 @@
 import React, { Component } from 'react';
 import styles from "../Components/Card.module.css";
 
+import styled from 'styled-components';
+
+const MyDiv1 = styled.div`
+
+display : block;
+justify-content: center;
+margin: 8px 12px;
+
+`
+
+const MyDiv2 = styled.div`
+
+display : flex;
+justify-content: center;
+margin-top: 60px;
+
+`
+
+const MyDiv3 = styled.div`
+
+display : flex;
+justify-content: center;
+margin-top: 20px;
+text-align : center;
+
+`
+
+const MyP1 = styled.p`
+display : flex;
+justify-content: center;
+font-size: 30px;
+
+`
+const MyP2 = styled.p`
+justify-content : center;
+color: black;
+fonr-size: 20px;
+font-weight: bold;
+`
+
+
 
 class Card extends Component {
     render() {
@@ -11,25 +52,24 @@ class Card extends Component {
                 <div style={{display: "flex", justifyContent: "center"}}><img src={image} alt='Product'></img></div>
                 <div><h3>{name}</h3></div>
                 <div className={styles.down}>
-                    <p style={{display: "flex", justifyContent: "center", fontSize: "30px"}}>$0.00</p>
+                    <MyP1>$0.00</MyP1>
                     <p>down</p>
                 </div>
-                <div style={{display: "flex", justifyContent: "center", marginTop: "20px", textAlign: "center"}}>
+                <MyDiv3>
                     <p className={styles.oldPrice}>${oldCost}</p>
                     <p className={styles.newPrice}>${newCost}/mo</p>
-                    
-                </div>
-                <div style={{justifyContent: "center", display: "block", margin:"8px 12px"}}>
+                </MyDiv3>
+                <MyDiv1>
                     <p style={{justifyContent:"center"}}>(After bill credit) on financing with </p>
                     <br></br>
-                    <p style={{justifyContent:"center", color:"black", fontSize:"20px" , fontWeight:"bold"}}>Upfront Edge</p>
+                    <MyP2>Upfront Edge</MyP2>
                     <br></br>
                     <p style={{fontSize:"13px", color:"gray"}}>Taxes Extra|24 mos|0% APR</p>
                     <p style={{fontSize:"15px", color:"909090"}}>Full price: $1,605.00</p>
-                    <div style={{display: "flex", justifyContent: "center", marginTop:"60px"}}>
-                        <button className={styles.viewDetails}>View details</button>
-                    </div>
-                </div>
+                    <MyDiv2>
+                         <button className={styles.viewDetails}>View details</button>
+                    </MyDiv2>
+                </MyDiv1>
             </div>
         );
     }
--- src/Components/Cards.js
+++ src/Components/Cards.js
@@ -5,17 +5,32 @@ import iphone14 from "../images/apple14.png";
 import iphone15 from "../images/apple15Green.png";
 import iphone15ProMax from "../images/apple15Pro-max.png";
 import iphone15Black from "../images/iPhone15Black.png";
+import styled from 'styled-components';
 
+const MyDiv1 = styled.div`
+display: flex;
+justify-content: space-around;
+margin: 60px;
+float: right;
+width: 80%;
+@media (max-width: 1200px){
+    display: block;
+    padding: 20px
+
+}
+`
 
 class Cards extends Component {
     render() {
         return (
-            <div className={styles.container}>
+            <>
+            <MyDiv1>
                 <Card image={iphone14} oldCost="30.00" newCost = "20.00" name= "Iphone 14"/>
                 <Card image={iphone15} oldCost="30.00" newCost = "20.00" name= "Iphone 15"/>
                 <Card image={iphone15ProMax} oldCost="42.50" newCost = "32.50" name= "Iphone 15 Pro Max"/>
                 <Card image={iphone15Black} oldCost="34.00" newCost = "24.00" name= "Iphone 15 Plus"/>
-            </div>
+            </MyDiv1>
+            </>
         );
     }
 }
